e83e422c9990c2d80a3751434fa4875987abbb39
Jack <jack.grahl@yahoo.co.uk>
2015-12-15 00:20:06 +0000

    Check that the url used by the request looks kind of right.

--- obnet/orders/test_orders.py
+++ obnet/orders/test_orders.py
@@ -1,10 +1,15 @@
 import mock
+import re
 import unittest
 
 from obnet.orders.orders import OrdersAll
 
 class TestOrders(unittest.TestCase):
-    @mock.patch('requests.get'):
+    url_regex = 'https://api.icbit.se/api/orders/all\?key=foo&signature=[A-Z0-9]+&nonce=[0-9]+'
+
+    @mock.patch('requests.get')
     def test_OrdersAll(self, patched):
         OrdersAll('foo', 'asdf', '123456')
         self.assertTrue(patched.called)
+        args = patched.call_args[0]
+        self.assertTrue(re.match(self.url_regex, args[0]), args[0])
